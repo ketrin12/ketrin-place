1e005a1e46651c60e42b9bbfe8afb0700dd159d8
ketrin12 <ivangritschevich357358@gmail.com>
2026-08-24 16:07:48 +0300

    Add files via upload

--- pages/1_Цены.py
+++ pages/1_Цены.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Цены | Ketrin Plase", layout="wide")
+st.set_page_config(page_title="Цены | Ketrin Plase", layout="wide", initial_sidebar_state="expanded")
 
 st.markdown("""
 <style>
